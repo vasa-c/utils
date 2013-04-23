25f88d643f654f12f258b9e6a9deb9a93b808f1a
Grigoriev Oleg <go.vasac@gmail.com>
2013-04-23 12:20:01 +0400

    backuper: config cli argument

--- php/backuper/backup.php
+++ php/backuper/backup.php
@@ -6,9 +6,33 @@
  * @author Григорьев Олег aka vasa_c <go.vasac@gmail.com>
  */
 
+$format = 'Format: backup.php [--config=CONFIG_FILE]';
+
+$args = $_SERVER['argv'];
+switch (\count($args)) {
+    case 1:
+        $configFilename = __DIR__.'/config.php';
+        break;
+    case 2:
+        if (!\preg_match('/^\-\-config\=(.*+)$/', $args[1], $matches)) {
+            echo $format.\PHP_EOL;
+            exit();
+        }
+        $configFilename = $matches[1];
+        break;
+    default:
+        echo $format.\PHP_EOL;
+        exit();
+}
+
+if (!\file_exists($configFilename)) {
+    echo 'Config file is not found'.\PHP_EOL;
+    exit();
+}
+$config = include($configFilename);
+
 require_once(__DIR__.'/Backuper/Autoloader.php');
 \Backuper\Autoloader::register();
 
-$config = include(__DIR__.'/config.php');
 $backuper = new \Backuper\Backuper($config);
 $backuper->run();
