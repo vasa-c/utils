8bb8f43daf8b1270244ac1e4688ff82150bc332b
Grigoriev Oleg <go.vasac@gmail.com>
2013-08-17 13:59:09 +0400

    const-string

---
+++ php/const-string/keys.php
@@ -0,0 +1,52 @@
+#!/usr/bin/env php
+<?php
+
+$count = 10000;
+
+echo 'PHP: '.\PHP_VERSION.\PHP_EOL;
+echo 'Count: '.$count.\PHP_EOL;
+
+$tags = array('one', 'two', 'three', 'four', 'five');
+
+$mem = \memory_get_usage();
+
+$mt = \microtime(true);
+$items = array();
+for ($i = 0; $i < $count; $i++) {
+    $items[] = array(
+        'tag' => $tags[$i % 5],
+        'value' => $i,
+    );
+}
+$mt = \microtime(true) - $mt;
+
+echo 'Create: '.$mt.\PHP_EOL;
+echo 'Mem: '.\number_format((\memory_get_usage() - $mem), 0, ',', ' ').\PHP_EOL;
+
+$sum = 0;
+$mt = \microtime(true);
+for ($i = 0; $i < 10; $i++) {
+    foreach ($items as $item) {
+        switch ($item['tag']) {
+            case 'one':
+                $sum += $item['value'];
+                break;
+            case 'two':
+                $sum += $item['value'] * 2;
+                break;
+            case 'three':
+                $sum += $item['value'] * 3;
+                break;
+            case 'four':
+                $sum += $item['value'] * 4;
+                break;
+            case 'five':
+                $sum += $item['value'] * 5;
+                break;                                                
+        }
+    }
+}
+$mt = \microtime(true) - $mt;
+
+echo 'Process: '.$mt.\PHP_EOL;
+
---
+++ php/const-string/numbers.php
@@ -0,0 +1,49 @@
+#!/usr/bin/env php
+<?php
+
+$count = 10000;
+
+echo 'PHP: '.\PHP_VERSION.\PHP_EOL;
+echo 'Count: '.$count.\PHP_EOL;
+
+$tags = array('one', 'two', 'three', 'four', 'five');
+
+$mem = \memory_get_usage();
+
+$mt = \microtime(true);
+$items = array();
+for ($i = 0; $i < $count; $i++) {
+    $items[] = array($i % 5, $i);
+}
+$mt = \microtime(true) - $mt;
+
+echo 'Create: '.$mt.\PHP_EOL;
+echo 'Mem: '.\number_format((\memory_get_usage() - $mem), 0, ',', ' ').\PHP_EOL;
+
+$sum = 0;
+$mt = \microtime(true);
+for ($i = 0; $i < 10; $i++) {
+    foreach ($items as $item) {
+        switch ($item[0]) {
+            case 0:
+                $sum += $item[1];
+                break;
+            case 1:
+                $sum += $item[1] * 2;
+                break;
+            case 2:
+                $sum += $item[1] * 3;
+                break;
+            case 3:
+                $sum += $item[1] * 4;
+                break;
+            case 4:
+                $sum += $item[1] * 5;
+                break;                                                
+        }
+    }
+}
+$mt = \microtime(true) - $mt;
+
+echo 'Process: '.$mt.\PHP_EOL;
+
